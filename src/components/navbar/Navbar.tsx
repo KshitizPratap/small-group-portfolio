9c516bf86e39d1f7af0d34ea48b6e8f2aeeada9e
kshitizPratap <kshitiz100mit@gmail.com>
2025-01-24 22:46:15 +0530

    Added portfolio CTA

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Signika } from "next/font/google";
 import Logo from "../../../public/logo.jpeg";
 import classes from "./navbar.module.scss";
+import Link from "next/link";
 
 const signika = Signika({
   subsets: ["latin"],
@@ -16,6 +17,9 @@ const Navbar = () => {
         <Image src={Logo} alt="Small Group" className={classes.logo} />
         <h1 className={signika.className}>Small Group</h1>
       </div>
+      <Link href="/portfolio" className={signika.className}>
+        Visit Portfolio
+      </Link>
     </div>
   );
 };
